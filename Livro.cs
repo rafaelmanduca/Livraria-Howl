5941c9e2cc8a488f77c49b0d0df941b3b478c789
Marcos <vimarcos188@gmail.com>
2024-07-16 16:04:19 -0300

    Class Livro

--- Livro.cs
+++ Livro.cs
@@ -8,5 +8,32 @@ namespace ConsoleApp1
 {
     internal class Livro
     {
+        public string Titulo { get; set; }
+        public string Editora { get; set; }
+        public string Autor { get; set; }
+        public int Preco { get; set; }
+
+
+        public Livro(string titulo, string autor, string editora, int preco)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Editora = editora;
+            Preco = preco;
+        }
+
+        public void ExibirLivros()
+        {
+            Console.WriteLine($"Título: {Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Editora: {Editora}");
+            Console.WriteLine($"Preco: {Preco}");
+        }
+
+        public void CadastrarLivro()
+        { Console.WriteLine($)}
+
+
+
     }
 }
